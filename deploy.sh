964044a3f2055fcd65806091dc1ad277ca8c1c5b
Udlei Nati <udlei@protonmail.ch>
2017-01-25 19:10:16 +0100

    ci: ignore circle_branch environment variable

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-export S3_FILE_PATH=app-ui/`date +"%Y-%m"`/$CIRCLE_BRANCH
+export S3_FILE_PATH=app-ui/`date +"%Y-%m"`
 export S3_FILE_NAME=commit-${CIRCLE_SHA1:0:7}.zip
-export S3_BUCKET=redirectpro-eb-$CIRCLE_BRANCH
-export EB_VERSION=$CIRCLE_BRANCH-${CIRCLE_SHA1:0:7}
+export S3_BUCKET=redirectpro-eb
+export EB_ENVIRONMENT=app-ui-development
+export EB_VERSION=commit-${CIRCLE_SHA1:0:7}
 export EB_APPLICATION=redirectpro-app-ui
-export EB_ENVIRONMENT=app-ui-$CIRCLE_BRANCH
 #
 # Setting commit revision
 sed -ie 's/LOCAL/'${CIRCLE_SHA1:0:7}'/g' package.json
@@ -13,7 +13,7 @@ npm run prestart
 #
 # Compressing and uploading file to S3
 zip -r -9 --exclude=node_modules/* $S3_FILE_NAME *
-aws s3 cp $S3_FILE_NAME s3://redirectpro-eb-$CIRCLE_BRANCH/$S3_FILE_PATH/
+aws s3 cp $S3_FILE_NAME s3://$S3_BUCKET/$S3_FILE_PATH/
 #
 # Creating EB Version
 aws elasticbeanstalk create-application-version \
