0723250e1518af944775251244a3b46e119d0635
Udlei Nati <udlei@protonmail.ch>
2017-05-19 18:54:01 +0200

    Deploy bug fix

--- src/environments/environment.tmp.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000',
-  auth0ClintId: 'h4pwXL5CnaV9xkA7YlJqsJojXqZhbp9L',
-  auth0Domain: 'keepat.auth0.com',
-  stripePublicKey: 'pk_test_gd8acLzybCR3OFkN5BQa8r5z',
-  redirectADestination: '127.0.0.1',
-  redirectCNAMEDestination: 'target.localhost'
-};
